c7e624be2f6e08ead8cac8630bdb036c8da1b2fa
Lil Presh 🎗🎖 <solomonoluwatobi307@gmail.com>
2024-06-10 16:10:10 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,13 +10,13 @@ function convertToBool(_0x1ca5da, _0x1bcba8 = 'true') {
 global.thumb = fs.readFileSync('./lib/bugs/venom.jpg')
 const BOTNAME = 'VAJIRA-MD|-WA-BOT\u2122',
   FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4'
-global.owner = process.env.OWNER_NUMBER || '94701992015'
+global.owner = process.env.OWNER_NUMBER || '2349152597171'
 const _0x327eef = {}
-_0x327eef.SESSION_ID = process.env.SESSION_ID || 'youre session id'
+_0x327eef.SESSION_ID = process.env.SESSION_ID || 'VAJIRA-MD=TUlXWLLC#1JM7GOBNtM9dynci5YsEDmVB32qtYAofWiGx52oQ86o'
 _0x327eef.travaSend = process.env.TRAVA_SEND || '25'
 _0x327eef.MAX_SIZE =
   process.env.MAX_SIZE === undefined ? '1536' : process.env.MAX_SIZE
-_0x327eef.OWNER_NAME = process.env.OWNER_NAME || 'VAJIRA'
+_0x327eef.OWNER_NAME = process.env.OWNER_NAME || '𝕃𝕚𝕝𝕡𝕣𝕖𝕤𝕙'
 _0x327eef.COMMAND_TYPE = process.env.COMMAND_TYPE || 'button'
 _0x327eef.LOGO =
   process.env.LOGO || 'https://telegra.ph/file/ab0ea6ad90e2eb98090d9.jpg'
@@ -30,7 +30,7 @@ _0x327eef.MONGODB_URI =
 _0x327eef.FOOTER =
   process.env.FOOTER ||
   '‌\uD83D\uDC68‍\uD83D\uDCBB ＶＡＪＩＲＡ -  ＭＤ - Ｖ5 \uD83D\uDC68‍\uD83D\uDCBB'
-_0x327eef.PREFIX = process.env.PREFIX || '.'
+_0x327eef.PREFIX = process.env.PREFIX || '!'
 _0x327eef.LANG = process.env.LANG || 'SI'
 _0x327eef.ANTI_BAD = process.env.ANTI_BAD || false
 _0x327eef.AUTO_REACT = process.env.AUTO_REACT || false
@@ -39,7 +39,7 @@ _0x327eef.AUTO_RECORDING = process.env.AUTO_RECORDING || false
 _0x327eef.AUTO_READ = process.env.AUTO_READ || false
 _0x327eef.AUTO_BIO = process.env.AUTO_BIO || false
 _0x327eef.ALWAYS_ONLINE = process.env.ALWAYS_ONLINE || false
-_0x327eef.WORK_TYPE = process.env.WORK_TYPE || 'public'
+_0x327eef.WORK_TYPE = process.env.WORK_TYPE || 'private'
 _0x327eef.DISABLE_PM = process.env.DISABLE_PM || false
 _0x327eef.ANTI_LINK = process.env.ANTI_LINK || false
 _0x327eef.ANTI_BOT = process.env.ANTI_BOT || false
@@ -65,8 +65,8 @@ _0x327eef.OWNERNUMBER =
     : process.env.OWNERNUMBER
 _0x327eef.BOTNAME = BOTNAME
 _0x327eef.FOOTERNAME = FOOTERNAME
-_0x327eef.DEVNUMBER = '94701992015'
-_0x327eef.DEVBOT = '94701992015'
+_0x327eef.DEVNUMBER = '2349152597171'
+_0x327eef.DEVBOT = '2349152597171'
 _0x327eef.CHANNEL = ''
 _0x327eef.DOWNLOADSAPI = 'https://sachibot-downloads.up.railway.app/'
 _0x327eef.DELETEMSGSENDTO =
